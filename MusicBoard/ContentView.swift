77a114ac3c44fa4580ef286835790391df9a142c
JimYang <jmyueng@yahoo.com>
2022-12-21 22:47:17 +0700

    add test of red background

--- MusicBoard/ContentView.swift
+++ MusicBoard/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, this is a test!")
+            Text("Hello, Redbackground this is a test!")
         }
         .padding()
     }
